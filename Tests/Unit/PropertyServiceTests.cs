8f36fd9ac860bbf1f790e1da7a7c891fd96dbd44
SeanS <seansong0829@gmail.com>
2024-10-19 00:35:45 +1300

    add more tests

---
+++ Tests/Unit/PropertyServiceTests.cs
@@ -0,0 +1,123 @@
+﻿using AutoMapper;
+using dashboardManger.Controllers;
+using dashboardManger.Data;
+using dashboardManger.Interfaces;
+using Moq;
+using Xunit;
+using System.Linq;
+using dashboardManger.Models;
+using dashboardManger.DTOs;
+using Microsoft.EntityFrameworkCore;
+
+namespace dashboardManger.Tests.Unit
+{
+    public class PropertyServiceTests
+    {
+        private readonly MyDbContext _context;
+        private readonly Mock<IPropertyService> _mockPropertyService;
+        private readonly PropertyController _propertyController;
+        private readonly IMapper _mapper;
+
+        public PropertyServiceTests()
+        {
+            _mockPropertyService = new Mock<IPropertyService>();
+
+            _propertyController = new PropertyController(_context, _mockPropertyService.Object, _mapper);
+        }
+
+        [Fact]
+        public void TestGetPropertyByGuid()
+        {
+            // Arrange
+            var propertyGuid = "9198696D-8F1C-4C47-B7E3-7ADF5582A4B2";
+
+            var property = new RealEstateProperty()
+            {
+                Address = "property1"
+            };
+
+            _mockPropertyService.Setup(x => x.GetPropertyByGuid(propertyGuid)).Returns(property);
+
+            // Act
+            var result = _propertyController.GetPropertyByGuid(propertyGuid);
+
+            // Assert
+            Assert.Equal(property.Address, result.Value.Address);
+        }
+
+
+        [Fact]
+        public void TestAddProperty()
+        {
+            // Arrange
+            var property = new PropertyDTO.PropertyUpdateSummary()
+            {
+                Address = "property1"
+            };
+
+            var newProperty = new RealEstateProperty()
+            {
+                Address = property.Address
+            };
+
+            _mockPropertyService.Setup(x => x.AddProperty(property)).Returns(newProperty);
+
+            // Act
+            var result = _propertyController.AddProperty(property);
+
+            // Assert
+            Assert.Equal(property.Address, result.Value.Address);
+        }
+
+        [Fact]
+        public void TestUpdateProperty()
+        {
+            // Arrange
+            var propertyGuid = "9198696D-8F1C-4C47-B7E3-7ADF5582A4B2";
+
+            var property = new RealEstateProperty()
+            {
+                Address = "property1"
+            };
+
+            _mockPropertyService.Setup(x => x.GetPropertyByGuid(propertyGuid)).Returns(property);
+
+            var propertyUpdate = new PropertyDTO.PropertyUpdateSummary()
+            {
+                Address = "property2"
+            };
+
+            var updatedPropertyResult = false;
+
+            _mockPropertyService.Setup(x => x.UpdateProperty(propertyUpdate)).Returns(updatedPropertyResult);
+
+            // Act
+            var result = _propertyController.UpdateProperty(propertyUpdate);
+        }
+
+
+        [Fact]
+        public void TestDeleteProperty()
+        {
+            // Arrange
+            var propertyGuid = "9198696D-8F1C-4C47-B7E3-7ADF5582A4B2";
+
+            var property = new RealEstateProperty()
+            {
+                Address = "property1"
+            };
+
+            _mockPropertyService.Setup(x => x.GetPropertyByGuid(propertyGuid)).Returns(property);
+
+            var deletedPropertyResult = false;
+
+            _mockPropertyService.Setup(x => x.DeleteProperty(propertyGuid)).Returns(deletedPropertyResult);
+
+            // Act
+            var result = _propertyController.DeleteProperty(propertyGuid);
+        }
+
+    }
+
+
+}
--- Tests/Unit/UserServiceTests.cs
+++ Tests/Unit/UserServiceTests.cs
@@ -4,8 +4,9 @@ using dashboardManger.Data;
 using dashboardManger.Interfaces;
 using Moq;
 using Xunit;
-using dashboardManger.Data;
+using System.Linq;
 using dashboardManger.Models;
+using dashboardManger.DTOs;
 
 namespace dashboardManger.Tests.Unit
 {
@@ -42,6 +43,230 @@ namespace dashboardManger.Tests.Unit
 
             // Assert
             Assert.Equal(user.Username, result.Value.Username);
+        }
+
+        [Fact]
+        public void TestGetCurrentUser()
+        {
+            // Arrange
+            var userGuid = "9198696D-8F1C-4C47-B7E3-7ADF5582A4B2";
+
+            var user = new UserDTO()
+            {
+               UserName = "admin"
+            };
+
+            _mockUserService.Setup(x => x.GetCurrentUser(userGuid)).Returns(user);
+
+            // Act
+            var result = _userController.GetCurrentUser();
+
+            // Assert
+            Assert.Equal(user.UserName, result.Value.UserName);
+        }
+
+        [Fact]
+        public void TestGetUsers()
+        {
+            // Arrange
+            var users = new List<User>()
+            {
+                new User()
+                {
+                    Username = "admin"
+                },
+                new User()
+                {
+                    Username = "user"
+                }
+            };
+
+            _mockUserService.Setup(x => x.GetAllUsers()).Returns(users);
+
+            // Act
+            var result = _userController.GetUsers();
+
+            // Assert
+            Assert.Equal(users.Count, result.Value.Count());
+        }
+
+        [Fact]
+        public void TestSimpleListUser()
+        {
+            // Arrange
+            var users = new List<UserDTO>()
+            {
+                new UserDTO()
+                {
+                    UserName = "admin"
+                },
+                new UserDTO()
+                {
+                    UserName = "user1"
+                }
+            };
+
+            _mockUserService.Setup(x => x.ListAllUsers()).Returns(users);
+
+            // Act
+            var result = _userController.SimpleListUser();
+
+            // Assert
+            Assert.Equal(users.Count, result.Value.Data.Count);
+           
+        }
+
+        [Fact]
+        public void TestAddUser()
+        {
+            // Arrange
+            var userUpdateSummary = new UserUpdateSummary()
+            {
+                UserName = "admin",
+                Email = "admin@localhost",
+                UserRoleId = 1,
+                StateId = 1,
+                DepartmentId = 1,
+                PhoneNumber = "123456789",
+                FirstName = "admin",
+                LastName = "admin",
+                Password = "admin"
+            };
+
+            var user = new User()
+            {
+                Username = "admin"
+            };
+
+            _mockUserService.Setup(x => x.AddUser(userUpdateSummary)).Returns(user);
+
+            // Act
+            var result = _userController.AddUser(userUpdateSummary);
+
+            // Assert
+            Assert.Equal(user.Username, result.Value.Username);
+        }
+
+        [Fact]
+        public void TestListAll()
+        {
+            // Arrange
+            var userSearchSummary = new UserSearchSummary()
+            {
+                FilterWord = "admin"
+            };
+
+            var users = new List<UserDTO>()
+            {
+                new UserDTO()
+                {
+                    UserName = "admin"
+                },
+                new UserDTO()
+                {
+                    UserName = "user1"
+                }
+            };
+
+            _mockUserService.Setup(x => x.ListAllUsers()).Returns(users);
+
+            // Act
+            var result = _userController.ListAll(userSearchSummary);
+
+            // Assert
+            Assert.Equal(users.Count, result.Value.Data.Count);
+        }
+
+
+        [Fact]
+        public void TestListAllWithFilter()
+        {
+            // Arrange
+            var userSearchSummary = new UserSearchSummary()
+            {
+                FilterWord = "admin"
+            };
+
+            var users = new List<UserDTO>()
+            {
+                new UserDTO()
+                {
+                    UserName = "admin"
+                },
+                new UserDTO()
+                {
+                    UserName = "user1"
+                }
+            };
+
+            _mockUserService.Setup(x => x.ListAllUsers()).Returns(users);
+
+            // Act
+            var result = _userController.ListAll(userSearchSummary);
+
+            // Assert
+            Assert.Equal(users.Count, result.Value.Data.Count);
+        }
+
+        [Fact]
+        public void TestListAllWithStateId()
+        {
+            // Arrange
+            var userSearchSummary = new UserSearchSummary()
+            {
+                StateId = 1
+            };
+
+            var users = new List<UserDTO>()
+            {
+                new UserDTO()
+                {
+                    UserName = "admin"
+                },
+                new UserDTO()
+                {
+                    UserName = "user1"
+                }
+            };
+
+            _mockUserService.Setup(x => x.ListAllUsers()).Returns(users);
+
+            // Act
+            var result = _userController.ListAll(userSearchSummary);
+
+            // Assert
+            Assert.Equal(users.Count, result.Value.Data.Count);
+        }
+
+        [Fact]
+        public void TestListAllWithStateIdAndFilter()
+        {
+            // Arrange
+            var userSearchSummary = new UserSearchSummary()
+            {
+                StateId = 1,
+                FilterWord = "admin"
+            };
+
+            var users = new List<UserDTO>()
+            {
+                new UserDTO()
+                {
+                    UserName = "admin"
+                },
+                new UserDTO()
+                {
+                    UserName = "user1"
+                }
+            };
+
+            _mockUserService.Setup(x => x.ListAllUsers()).Returns(users);
+
+            // Act
+            var result = _userController.ListAll(userSearchSummary);
+
+            // Assert
+            Assert.Equal(users.Count, result.Value.Data.Count);
         }
     }
 }
